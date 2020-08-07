5e7199afb8433c9c749954fda3d3b823e6c05906
sohyeonjin <shj41738866@gmail.com>
2020-08-07 17:13:38 +0900

    adding api to layout

--- src/App.js
+++ src/App.js
@@ -29,20 +29,73 @@ class App extends React.Component {
                      <span className="loder_text">Lodding...</span>
                  </div>
                  ):(
-                 <div className="movies">
-                 {movies.map(movie => (
-                    <Movie 
-                        key={movie.id} 
-                        id={movie.id} 
-                        year={movie.year} 
-                        title={movie.title} 
-                        summary={movie.summary} 
-                        poster={movie.medium_cover_image}
-                        genres={movie.genres}
-                    />
-                ))}
-                </div>
-                 )}
+                    <div className="App">
+                    <div id="wrap">
+                      <header>
+                          <h1><a href="#">Re-joy</a></h1>
+                          <div className="linker">
+                              <ul>
+                                 <li><a href="https://www.netflix.com/browse" target="_blank">넷플릭스</a></li>
+                                 <li><a href="https://play.watcha.net/" target="_blank">왓챠</a></li>
+                                 <li><a href="https://www.wavve.com/" target="_blank">웨이브</a></li>
+                             </ul>
+                         </div>
+                     </header>
+                     <nav>
+                         <div className="container">
+                             <div className="search">
+                                 {/*검색*/} 
+                                 <h2>search</h2>
+                                 {/*검색*/}
+                             </div>
+                             <div className="menu">
+                                 {/*메뉴 */}
+                                 <h2>menu</h2>
+                                 {/*메뉴*/}
+                             </div>
+                         </div>
+                     </nav>
+                     <div id="content">
+                         <div className="container">
+                             <div className="recommend">
+                                 {/*추천 */}
+                                 <section>
+                                    <h3 className="ir_su">recommend</h3>
+                                    <div className="movies">
+                                        {movies.map(movie => (
+                                            <Movie 
+                                                key={movie.id} 
+                                                id={movie.id} 
+                                                year={movie.year} 
+                                                title={movie.title} 
+                                                summary={movie.summary} 
+                                                poster={movie.medium_cover_image}
+                                                genres={movie.genres}
+                                            />
+                                        ))}
+                                    </div>
+                                 </section>
+                                 {/*추천 */}
+                             </div>
+                             <div className="category">
+                                 {/*분류 */}
+                                 <section>
+                                     <h3>분류</h3>
+                                 </section>
+                                 {/*분류 */}
+                             </div> 
+                         </div>
+                     </div>
+                     <footer>
+                         <div className="container">
+                             <div className="footer">
+                                 <h2>푸터</h2>
+                             </div>
+                         </div>
+                     </footer>
+                    </div>
+                 </div>                           
+                 )}                        
             </div>
         );
     }
--- src/Movie.js
+++ src/Movie.js
@@ -31,3 +31,17 @@ Movie.propTypes = {
 }
 
 export default Movie;
+
+{/* <div className="movies">
+                 {movies.map(movie => (
+                    <Movie 
+                        key={movie.id} 
+                        id={movie.id} 
+                        year={movie.year} 
+                        title={movie.title} 
+                        summary={movie.summary} 
+                        poster={movie.medium_cover_image}
+                        genres={movie.genres}
+                    />
+                ))}
+                </div> */}
