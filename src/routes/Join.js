ce3c84b733186921deb25d4a8cf56cfaf9ca5ef3
sohyeonjin <shj41738866@gmail.com>
2020-08-20 02:10:46 +0900

    update join&login page

--- src/routes/Join.js
+++ src/routes/Join.js
@@ -1,21 +1,37 @@
 import React from "react";
+import './Join.css';
 
 class Join extends React.Component {
     render(){
+        // state = {
+        //     userID:"",
+        //     userName:"",
+        //     userBirthday:"",
+        //     userEmail:"",
+        //     userAdress:""
+        // };
         return(
-            <div className="wrap">
-                <header>    
-                    <h1>회원가입</h1>
+            <div className="join_wrap">
+                {/* 헤더 */}
+                <header className="_header">    
+                    <h1>re-joy</h1>
                 </header>
-                <div id="content">
+                {/* //헤더 */}
+                {/* 컨텐츠 */}
+                <div className="join_content">
                     {/* 회원가입 입력 */}
-                    <div className="login_input">
-                        <input type="text" className="id_input" placeholder="아이디" maxLength="20"></input>
-                        <input type="text" className="pw_input" placeholder="비밀번호" maxLength="20"></input>
-                        <input type="submit" className="login_button"></input>
-                    {/* //회원가입 입력 */}
+                    <div className="join_input">
+                        <div><input type="text" className="id_input" placeholder="아이디" maxLength="20"/></div>
+                        <div><input type="password" className="pw_input" placeholder="비밀번호" maxLength="20"/></div>
+                        <div><input type="password" className="pw_input_check" placeholder="비밀번호 확인" maxLength="20"/></div>
+                        <div><input type="text" className="bd_input" placeholder="생년월일(yyyymmdd)" maxLength="20"/></div>
+                        <div><input type="email" className="email_input" placeholder="이메일" maxLength="20"/></div>
+                        <div><input type="text" className="adress_input" placeholder="주소" maxLength="20"/></div>
+                        <div><input type="submit" className="join_button" value="가입하기"/></div>
                     </div>
+                    {/* //회원가입 입력 */}
                 </div>
+                {/* 컨텐츠 */}
             </div>
         );
     }
--- src/routes/Login.js
+++ src/routes/Login.js
@@ -2,20 +2,28 @@ import React from 'react';
 
 class Login extends React.Component {
     render(){
+        // state = {
+        //     userID:"",
+        //     userPW:""
+        // };
         return(
-            <div className="wrap">
-                <header>    
-                    <h1>로그인</h1>
+            <div className="login_wrap">
+                {/* 헤더 */}
+                <header className="_header" style="margin-bottom:30px">    
+                    <h1>re-joy</h1>
                 </header>
-                <div id="content">
+                {/* //헤더 */}
+                {/* 컨텐츠 */}
+                <div className="join_content">
                     {/* 로그인 입력 */}
-                    <div className="login_input">
-                        <input type="text" className="id_input" placeholder="아이디" maxLength="20"></input>
-                        <input type="text" className="pw_input" placeholder="비밀번호" maxLength="20"></input>
-                        <input type="submit" className="login_button"></input>
-                    {/* //로그인 입력 */}
+                    <div className="join_input">
+                        <div><input type="text" className="id_input" placeholder="아이디" maxLength="20"/></div>
+                        <div><input type="password" className="pw_input" placeholder="비밀번호" maxLength="20"/></div>
+                        <div><input type="submit" className="join_button" value="로그인"/></div>
                     </div>
+                    {/* //로그인 입력 */}
                 </div>
+                {/* 컨텐츠 */}
             </div>
         );
     }
