078e8bb0d949059a0c09caa00c6e6c571bc08137
sohyeonjin <shj41738866@gmail.com>
2020-08-25 01:17:35 +0900

    update detail page

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -10,90 +10,124 @@ class Detail extends React.Component {
     if (location.state === undefined) {
       history.push("/");
     }
+    document.body.style.backgroundColor = "black";
+  }
+  
+  componentWillUnmount(){
+    document.body.style.backgroundColor = "#535962";
   }
 
   render() {
     const { location } = this.props;
     if (location.state) {
       return (
-        <div>
+        <div id="de_wrap">
           {/* 헤더 */}
-          <header>
-          <div className="header">
-                  {/* 헤더메뉴 */}
-                  <div className="header_top">
-                    <div className="container">
-                      <ul>
-                        <Link
-                          to={{
-                            pathname: "/Login",
-                            state: {},
-                          }}
-                        >
-                          <li className="login">로그인</li>
-                        </Link>
-                        <Link
-                          to={{
-                            pathname: "/Join",
-                            state: {},
-                          }}
-                        >
-                          <li className="join">회원가입</li>
-                        </Link>
-                        <Link
-                          to={{
-                            pathname: "/Mypage",
-                            state: {},
-                          }}
-                        >
-                          <li className="mypage">마이페이지</li>
-                        </Link>
-                      </ul>
-                    </div>
-                  </div>
-                  {/* //헤더메뉴 */}
+          <header id="de_header">
+            <div className="header">
+              {/* 헤더메뉴 */}
+              <div className="header_top">
+                <div className="container">
                   {/* 웹 이름 */}
-                  <div className="header_center">
-                    <div className="container">
-                      <Link
-                        style={{ textDecoration: "none" }}
-                        to={{
-                          pathname: "/",
-                        }}
-                      >
-                        <h1>re-joy</h1>
-                      </Link>
-                    </div>
-                  </div>
+                  <Link
+                    style={{ textDecoration: "none" }}
+                    to={{
+                      pathname: "/",
+                    }}
+                  >
+                    <h1>re-joy</h1>
+                  </Link>
                   {/* //웹 이름 */}
+                  <ul className="link">
+                    <Link
+                      to={{
+                        pathname: "/Login",
+                        state: {},
+                      }}
+                    >
+                      <li className="login">로그인</li>
+                    </Link>
+                    <Link
+                      to={{
+                        pathname: "/Join",
+                        state: {},
+                      }}
+                    >
+                      <li className="join">회원가입</li>
+                    </Link>
+                    <Link
+                      to={{
+                        pathname: "/Mypage",
+                        state: {},
+                      }}
+                    >
+                      <li className="mypage">마이페이지</li>
+                    </Link>
+                  </ul>
                 </div>
+              </div>
+              {/* //헤더메뉴 */}
+            </div>
           </header>
           {/* //헤더 */}
           <main id="de_main">
-            <div className="detail movie">
-              <img
-                className="poster"
-                src={"https://image.tmdb.org/t/p/w500" + location.state.poster}
-                alt={location.state.title}
-                title={location.state.title}
-              />
-              <div className="movie_data_wrap">
-              <div className="movie_data">
-                <h3 className="movie_title">{location.state.title}</h3>
-                <div className="movie_rating">
-                  <img
-                    className="star_icon"
-                    src={require("../img/star_icon.png")}
-                    alt="rating"
-                  />
-                  <h5 className="rating_text">{location.state.rating}</h5>
+            <div id="back_glass_l" className="glass"></div>
+            <div id="back_glass_r" className="glass"></div>
+            <div
+              id="back"
+              style={{
+                width: 1000,
+                height: 500,
+                backgroundImage:
+                  "url(https://image.tmdb.org/t/p/w500" +
+                  location.state.backdrop +
+                  ")",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "900px 500px",
+                backgroundPositionX: 50,
+                zIndex: 1,
+              }}
+            ></div>
+            <div id="de_movie">
+              <div className="detail movie">
+                <img
+                  className="poster"
+                  src={
+                    "https://image.tmdb.org/t/p/w500" + location.state.poster
+                  }
+                  alt={location.state.title}
+                  title={location.state.title}
+                />
+                <div className="movie_data_wrap">
+                  <div className="movie_data">
+                    <h3 className="movie_title">{location.state.title}</h3>
+                    <div className="de_movie_rating">
+                      <img
+                        className="star_icon"
+                        src={require("../img/star_icon.png")}
+                        alt="rating"
+                      />
+                      <h5 className="rating_text">평점:&emsp;{location.state.rating}</h5>
+                    </div>
+                    <h5 className="movie_year">개봉일:&emsp;{location.state.date}</h5>
+                    <p className="movie_summary">{location.state.summary}</p>
+                  </div>
                 </div>
-                <h5 className="movie_year">{location.state.date}</h5>
-                <p className="movie_summary">{location.state.summary}</p>
-              </div>
               </div>
             </div>
           </main>
+          <footer>
+            <div className="de_footer">
+              <div className="container">
+                <h2 className="ir_su">푸터</h2>
+                <span>re-joy</span>
+                <div>
+                  소현진&emsp; | &emsp;최유경&emsp; | &emsp;김형호&emsp; |
+                  &emsp;이유일
+                </div>
+              </div>
+            </div>
+          </footer>
         </div>
       );
     } else {
