ccca3e38810812872e14b99659d39897e0d3c91b
sohyeonjin <shj41738866@gmail.com>
2020-08-27 00:06:03 +0900

    add detail page

--- src/components/SearchRes.js
+++ src/components/SearchRes.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./reset.css";
 import "./Movie.css";
 
-function SearchRes({ id, title, media_type, name, poster, summary, genres, date, rating }) {
+function SearchRes({ id, title, media_type, name, poster, backdrop, summary, genres, adult, date, rating }) {
    var cont_title = "";
    if (media_type === "movie") {
      cont_title = title;
@@ -11,25 +12,32 @@ function SearchRes({ id, title, media_type, name, poster, summary, genres, date,
     cont_title = name;
    }
   return (
-    <div className="movie" style={{ marginLeft: 13, marginRight: 13 }}>
-      <img
-        className="poster"
-        src={"https://image.tmdb.org/t/p/w500" + poster}
-        alt={cont_title}
-        title={cont_title}
-      />
-      <div className="movie_data">
-        <h3 className="movie_title" style={{color:"black"}}>{cont_title}</h3>
-        <div className="movie_rating">
-          <img
-            className="star_icon"
-            src={require("../img/star_icon.png")}
-            alt="rating"
-          />
-          <h5 className="rating_text"  style={{color:"black"}}>{rating}</h5>
+    <Link
+        to={{
+          pathname: "/Detail/" + title,
+          state: { id, cont_title, poster, backdrop, summary, genres, adult, date, rating },
+        }}
+      >
+      <div className="movie" style={{ marginLeft: 13, marginRight: 13 }}>
+        <img
+          className="poster"
+          src={"https://image.tmdb.org/t/p/w500" + poster}
+          alt={cont_title}
+          title={cont_title}
+        />
+        <div className="movie_data">
+          <h3 className="movie_title" style={{color:"black"}}>{cont_title}</h3>
+          <div className="movie_rating">
+            <img
+              className="star_icon"
+              src={require("../img/star_icon.png")}
+              alt="rating"
+            />
+            <h5 className="rating_text"  style={{color:"black"}}>{rating}</h5>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
