e512a10840ca655c385326fed6b25a080ae16503
sohyeonjin <shj41738866@gmail.com>
2020-08-26 20:48:41 +0900

    add movie detail page to tvshow

--- src/components/Tvshow.js
+++ src/components/Tvshow.js
@@ -1,41 +1,49 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./reset.css";
 import "./Movie.css";
 
-function Tvshow({ id, title, poster, summary, genres, date, rating }) {
+function Tvshow({ id, title, poster, backdrop, summary, genres, adult, date, rating }) {
   return (
     <div className="space_around">
-      <div className="movie">
-        <img
-          className="poster"
-          src={"https://image.tmdb.org/t/p/w500" + poster}
-          alt={title}
-          title={title}
-        />
-        <div className="movie_data">
-          <h3 className="movie_title">
-            {title}
-          </h3>
-          <div className="movie_rating">
-            <img
-              className="star_icon"
-              src={require("../img/star_icon.png")}
-              alt="rating"
-            />
-            <h5 className="rating_text">{rating}</h5>
+      <Link
+        to={{
+          pathname: "/Detail/" + title,
+          state: { id, title, poster, backdrop, summary, genres, adult, date, rating },
+        }}
+      >
+        <div className="movie">
+          <img
+            className="poster"
+            src={"https://image.tmdb.org/t/p/w500" + poster}
+            alt={title}
+            title={title}
+          />
+          <div className="movie_data">
+            <h3 className="movie_title">
+              {title}
+            </h3>
+            <div className="movie_rating">
+              <img
+                className="star_icon"
+                src={require("../img/star_icon.png")}
+                alt="rating"
+              />
+              <h5 className="rating_text">{rating}</h5>
+            </div>
+            {/*<h5 className="movie_year">{date}</h5>*/}
+            {/*<ul className="genres">
+                  {genres.map((genre, index) => (
+                  <li key={index} className="genres_genre">
+                      {genre}
+                  </li>
+              ))}
+              </ul>*/}
+            {/* <p className="movie_summary">{summary}</p> */}
           </div>
-          {/*<h5 className="movie_year">{date}</h5>*/}
-          {/*<ul className="genres">
-                {genres.map((genre, index) => (
-                <li key={index} className="genres_genre">
-                    {genre}
-                </li>
-             ))}
-            </ul>*/}
-          {/* <p className="movie_summary">{summary}</p> */}
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
