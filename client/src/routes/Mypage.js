40284d117f41a55f0a901220a8ecc609625d7844
sohyeonjin <shj41738866@gmail.com>
2020-08-31 23:32:51 +0900

    add mypage by H-ho

--- client/src/routes/Mypage.js
+++ client/src/routes/Mypage.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React from "react";
 import "./Mypage.css";
 
-function Mypage(){
-    return <span>this page is for Mypage</span>
+function Mypage() {
+  return (
+    <div id="mypage_wrap">
+      <body>
+        {/* 즐겨찾기 체크된 영화 표시  */}
+        <p>
+          <h1>나의 즐겨찾기 콘텐츠</h1>
+          <div class="container">
+            <button> </button>
+            <div class="starlist">image</div>
+            <button> </button>
+          </div>
+        </p>
+
+        <p>
+          <h1>설정</h1>
+          <div class="setting">
+            <p>
+              <a href="">계정관리</a>
+            </p>
+            <p>
+              <a href="">환경설정</a>
+            </p>
+          </div>
+        </p>
+      </body>
+    </div>
+  );
 }
 
 export default Mypage;
