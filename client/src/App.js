9fccf524f29432659d6d9c6e19706029f95f7628
sohyeonjin <shj41738866@gmail.com>
2020-11-13 00:57:45 +0900

    modefied design

--- client/src/App.js
+++ client/src/App.js
@@ -6,19 +6,21 @@ import Mypage from "./routes/Mypage";
 import Login from "./routes/Login";
 import Search from "./routes/Search";
 import Detail from "./routes/Detail";
-import Nav from "./components/view/Navbar"
-import Footer from "./components/view/Footer"
+import Nav from "./components/view/Navbar";
+import Footer from "./components/view/Footer";
 
 function App() {
   return (
     <HashRouter>
       <Nav />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/Mypage" component={Mypage} />
-      <Route exact path="/Join" component={Join} />
-      <Route exact path="/Login" component={Login} />
-      <Route exact path="/Search/:query" component={Search} />
-      <Route exact path="/Detail/:title" component={Detail} />
+      <div style={{ minHeight: "calc(100vh - 130px)" }}>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/Mypage' component={Mypage} />
+        <Route exact path='/Join' component={Join} />
+        <Route exact path='/Login' component={Login} />
+        <Route exact path='/Search/:query' component={Search} />
+        <Route exact path='/Detail/:title' component={Detail} />
+      </div>
       <Footer />
     </HashRouter>
   );
